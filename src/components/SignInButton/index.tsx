1ef09ff30e69db37abc1b749e5bfa74954de0361
Augusto Kestering Longo <augusto.kestering@gmail.com>
2021-09-07 17:50:35 -0300

    Added fauna support

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,25 +7,25 @@ import styles from './styles.module.scss';
 export function SignInButton() {
   const [ session ] = useSession();
 
-  return session ? 
-  ( 
-    <button 
-    type="button"
-    className={styles.signInButton}
-    onClick={() => signOut()}
-    >
-      <FaGithub color="#04d361"/>
-      {session.user.name}
-      <FiX color="#737380" className={styles.closeIcon}/>
-    </button>
-  ) : (
-    <button 
-    type="button"
-    className={styles.signInButton}
-    onClick={() => signIn('github')}
-    >
-      <FaGithub color="#eba417"/>
-      Sign in with GitHub
-    </button>
-)
+  return session ?
+    (
+      <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+      >
+        <FaGithub color="#04d361"/>
+        {session.user.name}
+        <FiX color="#737380" className={styles.closeIcon}/>
+      </button>
+    ) : (
+      <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+      >
+        <FaGithub color="#eba417"/>
+        Sign in with GitHub
+      </button>
+  )
 }
