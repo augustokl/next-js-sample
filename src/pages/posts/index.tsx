710915d505ef1b5ce38757a7b76073e0358e162d
Augusto Kestering Longo <augusto.kestering@gmail.com>
2021-09-09 22:11:14 -0300

    Finish project

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link'
+import { useSession } from 'next-auth/client';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-reactjs'
 
@@ -19,6 +20,8 @@ interface PostsProps {
 }
 
 export default function Posts({ posts }: PostsProps) {
+  const [session] = useSession();
+
   return (
     <>
       <Head>
@@ -28,7 +31,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           { posts.map(post => (
-            <Link href={`/posts/${post.slug}`} key={post.slug}>
+            <Link 
+              href={session?.activeSubscription 
+                ? `/posts/${post.slug}` 
+                : `/posts/preview/${post.slug}`
+              } 
+              key={post.slug}
+            >
               <a>
                 <time>{post.updatedAt}</time>
                 <strong>{post.title}</strong>
---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,88 @@
+import { GetStaticPaths, GetStaticProps } from "next"
+import { useSession } from "next-auth/client";
+import Head from 'next/head';
+import Link from 'next/link'
+import { useRouter } from "next/router";
+import { RichText, RichTextBlock } from "prismic-reactjs";
+import { useEffect } from "react";
+
+import { getPrismicClient } from "../../../services/prismic";
+
+import styles from '../post.module.scss';
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: RichTextBlock[];
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps) {
+  const [session] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if(session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`)
+    }
+  }, [post.slug, router, session])
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} | ig.news</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div className={`${styles.postContent} ${styles.previewContent}`}>
+            <RichText render={post.content} />
+          </div>
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a> Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params
+
+  const prismic = getPrismicClient()
+
+  const response = await prismic.getByUID(`post`, String(slug), {})
+
+  const post = {
+      slug: slug,
+      title: RichText.asText(response.data.title),
+      content: response.data.content.splice(0, 3),
+      updatedAt: new Date(response.last_publication_date).toLocaleDateString(response.lang, {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+
+  return {
+    props: {
+      post
+    },
+    revalidate: 60 * 30
+  }
+}
